e84d9a097d3d6b962a1d672497e7e2f27cda9d47
victorruan <250069802@qq.com>
2017-08-24 22:29:24 +0800

    修改nav

--- project/frontend/config/params.php
+++ project/frontend/config/params.php
@@ -3,6 +3,7 @@ return [
     'adminEmail' => 'sb@oonne.com',
     'nav'=>[
         'items'=>[
+            ['label' => "about-me", 'url' => 'http://about-me.victorruan.cn/','linkOptions'=>['class'=>"navbar-aboutme" ,"target"=>"_blank"]],
             ['label' => "github", 'url' => 'https://github.com/victorruan','linkOptions'=>['class'=>"navbar-github" ,"target"=>"_blank"]],
             ['label' => "segmentfault", 'url' => 'https://segmentfault.com/u/victorruan','linkOptions'=>['class'=>"navbar-segmentfault" ,"target"=>"_blank"]],
         ]
