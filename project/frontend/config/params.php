41ca338891b27ed33a2da5718716f79f291e2276
victorruan <250069802@qq.com>
2017-08-24 17:49:45 +0800

    修改nav

--- project/frontend/config/params.php
+++ project/frontend/config/params.php
@@ -3,7 +3,7 @@ return [
     'adminEmail' => 'sb@oonne.com',
     'nav'=>[
         'items'=>[
-            ['label' => "github", 'url' => 'https://github.com/victorruan','options'=>['class'=>"navbar-github" ,"target"=>"_blank"] ]
+            ['label' => "github", 'url' => 'https://github.com/victorruan','linkOptions'=>['class'=>"navbar-github" ,"target"=>"_blank"]]
         ]
     ]
 ];
